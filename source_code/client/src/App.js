470e7b8b48c19975fbd8b51b7fc97a5928a0b514
Anitha <anithabapathu14@gmail.com>
2024-03-24 17:08:38 -0400

    Changes to App.js

--- source_code/client/src/App.js
+++ source_code/client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Hp from "./Home";
 import Pp from "./Myprofile";
 import Ap from "./Myadd";
+import Ip from "./MyInventory";
+import Apip from "./MyAPI";
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
           <Route path="/" element={<Hp />} />
           <Route path="/MyProfile" element={<Pp />} />
           <Route path="/Myadd" element={<Ap />} />
+          <Route path="/MyInventory" element={<Ip />}/>
+          <Route path="/MyAPI" element={<Apip />}/>
         </Routes>
       </Router>
     </div>
