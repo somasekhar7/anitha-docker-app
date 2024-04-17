eb066abc5354b29df84a201fccc5e3aa4b492d26
somasekhar7 <somasekhardarisi7@gmail.com>
2024-04-17 02:10:56 -0400

    updated

--- source_code/client/src/constants.js
+++ source_code/client/src/constants.js
@@ -1,2 +1,2 @@
 export const BACKEND_API_ENDPOINT =
-  process.env.BACKEND_API_ENDPOINT || "http://localhost";
+  process.env.BACKEND_API_ENDPOINT || "http://ec2-54-86-83-147.compute-1.amazonaws.com";
